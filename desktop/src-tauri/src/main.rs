78af745e2173056deee748007f208b4aac6740d0
laptop <laptop@notreal.com>
2025-08-06 08:27:26 +0300

    test: Create minimal build to isolate crash issue
    - Disable most plugins except essential ones for hotkey testing
    - Use minimal setup function to avoid complex initialization
    - Keep only hotkey commands to test if they work
    - This should help identify if crash is in core app or specific plugins

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -41,68 +41,19 @@ fn main() -> Result<()> {
     cli::attach_console();
 
     tauri::Builder::default()
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-            tracing::debug!("{}, {argv:?}, {cwd}", app.package_info().name);
-            if let Some(webview) = app.get_webview_window("main") {
-                webview.set_focus().map_err(|e| eyre!("{:?}", e)).log_error();
-            }
-            app.emit("single-instance", argv).map_err(|e| eyre!("{:?}", e)).log_error();
-        }))
-        .setup(|app| setup::setup(app))
-        .plugin(
-            tauri_plugin_window_state::Builder::default()
-                .with_state_flags(!StateFlags::VISIBLE)
-                .build(),
-        )
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::default().build())
-        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_keepawake::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .setup(|_app| {
+            println!("App setup started");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
-            cmd::download_file,
-            cmd::get_cargo_features,
-            cmd::transcribe,
-            cmd::glob_files,
-            cmd::download_model,
-            cmd::load_model,
-            cmd::get_commit_hash,
-            cmd::get_cuda_version,
-            cmd::get_rocm_version,
-            cmd::is_avx2_enabled,
-            cmd::is_online,
-            cmd::get_path_dst,
-            cmd::get_logs,
-            cmd::open_path,
-            cmd::get_x86_features,
-            cmd::get_save_path,
-            cmd::get_argv,
-            cmd::audio::get_audio_devices,
-            cmd::audio::start_record,
-            cmd::get_models_folder,
-            cmd::is_portable,
-            cmd::check_vulkan,
-            cmd::get_logs_folder,
-            cmd::show_log_path,
-            cmd::show_temp_path,
-            cmd::get_ffmpeg_path,
-            cmd::ytdlp::download_audio,
-            cmd::ytdlp::get_temp_path,
-            cmd::is_crashed_recently,
-            cmd::rename_crash_file,
             cmd::hotkey::register_hotkeys,
             cmd::hotkey::unregister_hotkeys,
             cmd::hotkey::check_hotkey_availability,
-            cmd::hotkey::get_registered_hotkeys,
-            #[cfg(windows)]
-            cmd::set_high_gpu_preference
+            cmd::hotkey::get_registered_hotkeys
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
